7edc0b3e62110ee16ef56aff3583715ee854ccc3
Dan Chilton <dan.chilton@gmail.com>
2011-05-25 18:05:17 -0400

    Add delicious and digg functionality

--- TwitBook.js
+++ TwitBook.js
@@ -1,7 +1,3 @@
-$(document).ready(function(){
-	$('.tb-facebook, .tb-twitter').TwitBook();
-});
-
 (function($){
   $.fn.TwitBook = function() {
     this.each(function(){
@@ -14,15 +10,18 @@ $(document).ready(function(){
     	{
     	if 			(type=='fb') { json_url = 'http://graph.facebook.com/?ids='+url; }
     	else if	(type=='tw') { json_url = 'http://api.tweetmeme.com/url_info.jsonc?url='+url; }
+    	else if	(type=='dl') { json_url = 'http://feeds.delicious.com/v2/json/urlinfo/data?url='+url; }
+    	else if	(type=='dg') { json_url = 'http://services.digg.com/1.0/endpoint?method=story.getAll&amp;link='+url+'&type=javascript'; }
     	$.ajax({
 				url: json_url,
 				dataType: 'jsonp',
 				success: function(data) {
 					if 			(type=='fb') { $count.text(data[url].shares); }
 					else if	(type=='tw') { $count.text(data['story'].url_count); }
+					else if	(type=='dl') { $count.text(data[0].total_posts); }
+					else if	(type=='dg') { $count.text(data.stories[0].diggs); }
 					}
 				});
-				
     	}
   };
 })( jQuery );
--- example.php
+++ example.php
@@ -3,6 +3,11 @@
 	<title>TwitBook Test</title>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
 	<script type="text/javascript" src="TwitBook.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+  	$('.tb-facebook, .tb-twitter').TwitBook();
+  });
+  </script>
 </head>
 <body>
 
@@ -13,6 +18,8 @@
 		<div class="social">
 			<div class="facebook tb-facebook" data-tb-url="http://www.thisismynext.com">Facebook has <span class="tb-count"></span> likes</div>
 			<div class="twitter tb-twitter" data-tb-url="http://www.thisismynext.com">Twitter has <span class="tb-count"></span> tweets</div>
+			<div class="facebook tb-delicious" data-tb-url="http://www.thisismynext.com">Delicious has <span class="tb-count"></span> bookmarks</div>
+			<div class="twitter tb-digg" data-tb-url="http://www.thisismynext.com">Digg has <span class="tb-count"></span> diggs</div>
 		</div>
 	</div>
 	
@@ -21,6 +28,8 @@
 		<div class="social">
 			<div class="facebook tb-facebook" data-tb-url="http://www.sbnation.com">Facebook has <span class="tb-count"></span> likes</div>
 			<div class="twitter tb-twitter" data-tb-url="http://www.sbnation.com">Twitter has <span class="tb-count"></span> tweets</div>
+			<div class="facebook tb-delicious" data-tb-url="http://www.sbnation.com">Delicious has <span class="tb-count"></span> bookmarks</div>
+			<div class="twitter tb-digg" data-tb-url="http://www.sbnation.com">Digg has <span class="tb-count"></span> diggs</div>
 		</div>
 	</div>
 	
@@ -29,6 +38,8 @@
 		<div class="social">
 			<div class="facebook tb-facebook" data-tb-url="http://mlb.sbnation.com">Facebook has <span class="tb-count"></span> likes</div>
 			<div class="twitter tb-twitter" data-tb-url="http://mlb.sbnation.com">Twitter has <span class="tb-count"></span> tweets</div>
+			<div class="facebook tb-delicious" data-tb-url="http://mlb.sbnation.com">Delicious has <span class="tb-count"></span> bookmarks</div>
+			<div class="twitter tb-digg" data-tb-url="http://mlb.sbnation.com">Digg has <span class="tb-count"></span> diggs</div>
 		</div>
 	</div>
 	
@@ -37,6 +48,8 @@
 		<div class="social">
 			<div class="facebook tb-facebook" data-tb-url="http://www.joshuatopolsky.com">Facebook has <span class="tb-count"></span> likes</div>
 			<div class="twitter tb-twitter" data-tb-url="http://www.joshuatopolsky.com">Twitter has <span class="tb-count"></span> tweets</div>
+			<div class="facebook tb-delicious" data-tb-url="http://www.joshuatopolsky.com">Delicious has <span class="tb-count"></span> bookmarks</div>
+			<div class="twitter tb-digg" data-tb-url="http://www.joshuatopolsky.com">Digg has <span class="tb-count"></span> diggs</div>
 		</div>
 	</div>
 	
