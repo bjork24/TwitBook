d6464ce15008522ba4f51c40967d4d3dd93c47c1
Dan Chilton <dan.chilton@gmail.com>
2011-05-25 16:18:29 -0700

    Made the tabs look better.

--- example.php
+++ example.php
@@ -4,10 +4,10 @@
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
 	<script type="text/javascript" src="TwitBook.min.js"></script>
 	<script type="text/javascript">
-  	$(document).ready(function(){
-    	$('.tb-link').TwitBook();
-    });
-  </script>
+		$(document).ready(function(){
+			$('.tb-link').TwitBook();
+		});
+	</script>
 </head>
 <body>
 
